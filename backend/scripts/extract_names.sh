03cf9e653f0e414d748be1dd95e46693a9892d44
Shem <smagnezi8@gmail.com>
2022-10-27 13:40:18 +0300

    Update extract_names.sh

--- backend/scripts/extract_names.sh
+++ backend/scripts/extract_names.sh
@@ -3,6 +3,11 @@
 echo $1
 echo $2
 
+cat $1 
+cat $1 | grep -iF "@amazon.com!"
+cat $1 | grep -iF "@amazon.com!" | awk -F , '{print $3" "$2}'
+
 cat $1 | grep -iF "@amazon.com!" | awk -F , '{print $3" "$2}' > $2
 
+echo "printing files"
 cat $2
